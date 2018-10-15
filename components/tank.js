28501edf59c0edf6b39ae6d8d3554f77cb6b2d60
James Rumack <james.rumack@klarna.com>
2018-10-15 08:08:26 +0200

    Some visual tweaks to the temperatures

--- components/tank.js
+++ components/tank.js
@@ -1,15 +1,30 @@
+import moment from 'moment'
 export default ({ label = '', temperatures = [] }) => (
   <div className='container'>
     <style jsx>{`
       .container {
-        margin: 10px
+        margin: 10px;
+      }
+      .label {
+        margin-block-end: 0.2em; 
+        font-size: 1em;
+      }
+      .number {
+        font-size: 1.4em;
+        font-weight: bold;
       }
     `}</style>
     <h2>{label}</h2>
     {temperatures.map(temperature => (
       <div>
         {temperature &&
-          <span className='row'>{`${temperature.label}: ${temperature.temperature} °C`}</span>}
+          <div>
+            <div className='stats'>
+              <h3 className='label'>{`${temperature.label}: `}</h3>
+              <span className='number'>{`${temperature.temperature} °C`}</span>
+              <span> {moment(temperature.createdAt).fromNow()}</span>
+            </div>
+          </div>}
       </div>
     ))}
   </div>
